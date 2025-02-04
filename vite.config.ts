3b301893f34a75b639c00821abc51453b8e90c37
Th3FenrisWolf <34248224+Th3FenrisWolf@users.noreply.github.com>
2025-02-03 20:17:51 -0500

    Update Vite Config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Vue-Playground/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
