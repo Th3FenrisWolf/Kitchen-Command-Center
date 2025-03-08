57b38250c061b3d8f11c81d87f738ec18d4bc96f
Th3FenrisWolf <34248224+Th3FenrisWolf@users.noreply.github.com>
2025-03-08 18:59:33 -0500

    Feature/firebase (#11)
    * Add Firebase Auth
    
    * Add Profile Page and Sign In / Sign Out Routing

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,1 +1,42 @@
-<template><div></div></template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { signIn, signUp } from '~/utilities/auth'
+
+const signInEmail = ref('')
+const signInPassword = ref('')
+const signUpEmail = ref('')
+const signUpPassword = ref('')
+</script>
+
+<template>
+  <div>
+    <h1>Sign In / Sign Up</h1>
+    <form
+      @submit="
+        (e) => {
+          e.preventDefault()
+          signIn(signInEmail, signInPassword)
+        }
+      "
+    >
+      <h2>Sign In</h2>
+      <input type="email" v-model="signInEmail" placeholder="Email" />
+      <input type="password" v-model="signInPassword" placeholder="Password" />
+      <button type="submit">Sign In</button>
+    </form>
+
+    <form
+      @submit="
+        (e) => {
+          e.preventDefault()
+          signUp(signUpEmail, signUpPassword)
+        }
+      "
+    >
+      <h2>Sign Up</h2>
+      <input type="email" v-model="signUpEmail" placeholder="Email" />
+      <input type="password" v-model="signUpPassword" placeholder="Password" />
+      <button type="submit">Sign Up</button>
+    </form>
+  </div>
+</template>
