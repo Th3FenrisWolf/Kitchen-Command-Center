ac00e061f0c2915c1da7ed0fb4b178fc95825d86
Th3FenrisWolf <34248224+Th3FenrisWolf@users.noreply.github.com>
2025-04-23 22:34:13 -0400

    Update FontAwesome Syntax

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faGripLines, faTrash, faSquarePen } from '@fortawesome/free-solid-svg-icons'
 import router from '~/router'
 import { initializeFirebase } from '~/utilities/firebase'
 import App from '~/App.vue'
-
-library.add(faGripLines, faTrash, faSquarePen)
 
 const app = createApp(App)
 app.use(createPinia())
