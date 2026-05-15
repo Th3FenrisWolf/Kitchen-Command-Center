ea999494a830642eb47b95e3550067626a33b544
Th3FenrisWolf <34248224+Th3FenrisWolf@users.noreply.github.com>
2026-05-15 10:07:50 -0400

    Track Resource String Keys via CI/CD

--- src/KCC.ResourceStrings/Data/ResourceStringInfo.cs
+++ src/KCC.ResourceStrings/Data/ResourceStringInfo.cs
@@ -2,6 +2,12 @@ namespace KCC.ResourceStrings.Data;
 
 public partial class ResourceStringInfo
 {
+    static ResourceStringInfo()
+    {
+        TYPEINFO.ContinuousIntegrationSettings.Enabled = true;
+        TYPEINFO.SerializationSettings.ExcludedFieldNames.Add(nameof(ResourceStringValue));
+    }
+
     public string Key => ResourceStringKey;
 
     public string Value => ResourceStringValue;
