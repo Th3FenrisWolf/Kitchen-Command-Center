76313d622b638d9cc54f299a31dd48e2df48d224
Th3FenrisWolf <34248224+Th3FenrisWolf@users.noreply.github.com>
2026-09-16 12:46:20 -0400

    feat(footer): desk chrome

--- src/KCC.Web/Features/Components/Footer/AppFooter.Component.vue
+++ src/KCC.Web/Features/Components/Footer/AppFooter.Component.vue
@@ -10,7 +10,7 @@
 <!-- #endregion -->
 
 <template>
-  <footer class="content-grid mt-8 bg-paper p-4 text-center text-sm">
-    <p class="text-ink">&copy; 2025</p>
+  <footer class="content-grid mt-8 p-4 text-center text-sm">
+    <p class="text-ink-soft">&copy; 2025</p>
   </footer>
 </template>
